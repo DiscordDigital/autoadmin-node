f642ef7271bd2602b3023f09610b9e9e43205b50
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2022-09-08 20:29:34 +0200

    Update autoadmin.js

--- autoadmin.js
+++ autoadmin.js
@@ -1,15 +1,15 @@
 module.exports.autoadmin = async (a = {}) => {
     return new Promise((resolve) => {
         const { spawn } = require('child_process');
-        const os = require('os')
-        const argTemplate = {action: undefined, command: undefined, user: undefined}
+        const os = require('os');
+        const argTemplate = {action: undefined, command: undefined, user: undefined};
         const allArgKeys = Object.keys(a);
         let args = {};
         for (const arg of allArgKeys) {
             arg in a ? args[arg] = a[arg] : args[arg] = argTemplate[arg];
         }
         const escape = (arr) => {
-            if (arr.length === 0) return
+            if (arr.length === 0) return;
             col = [];
             arr.forEach((string)=>{
                 const regex1 = /[^a-zA-Z\d\s_:\/&-]/g;
@@ -20,36 +20,36 @@ module.exports.autoadmin = async (a = {}) => {
             return col.join(' ');
         }
         const runAndResolve = (command) => {
-            const execution = spawn(command, [], {cwd: process.env.USERPROFILE, shell: true})
+            const execution = spawn(command, [], {cwd: process.env.USERPROFILE, shell: true});
 
             execution.stderr.on('data', (data) => {
                 console.error(`${data}`);
-                resolve(false)
+                resolve(false);
             });
 
             execution.on('close', (code) => {
-                resolve(true)
+                resolve(true);
             });
         }
-        switch(args['action']) { 
+        switch(args['action']) {
             case undefined:
-                console.error('No action was passed.')
-                resolve(false)
+                console.error('No action was passed.');
+                resolve(false);
                 break;
             case 'hook':
                 if (args['command'] === undefined) {
-                    console.error('No command was specified.')
-                    resolve(false)
+                    console.error('No command was specified.');
+                    resolve(false);
                 }
-                const command = args['command']
-                const user = args['user'] === undefined ? os.userInfo().username : args['user']
-                runAndResolve(`schtasks /create /tn "autoadmin" /tr ${escape([command])} /sc ONLOGON /IT /ru ${escape([user])} /RL HIGHEST /F`)
+                const command = args['command'];
+                const user = args['user'] === undefined ? os.userInfo().username : args['user'];
+                runAndResolve(`schtasks /create /tn "autoadmin" /tr ${escape([command])} /sc ONLOGON /IT /ru ${escape([user])} /RL HIGHEST /F`);
                 break;
             case 'unhook':
-                runAndResolve(`schtasks /Delete /tn "autoadmin" /F`)
+                runAndResolve(`schtasks /Delete /tn "autoadmin" /F`);
                 break;
             case 'run':
-                runAndResolve(`schtasks /run /tn "autoadmin"`)
+                runAndResolve(`schtasks /run /tn "autoadmin"`);
                 break;
         }
     });

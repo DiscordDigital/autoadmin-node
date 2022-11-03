a18d60d84945ac816468efe15eaebb5025308d09
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2022-11-03 10:30:18 +0100

    Update autoadmin.js
    Added the option to define an id, which defaults to 'default', which can be used to create multiple autoadmin entries. Autostart can now be disabled by defining autostart as false.

--- autoadmin.js
+++ autoadmin.js
@@ -2,7 +2,7 @@ module.exports.autoadmin = async (a = {}) => {
     return new Promise((resolve) => {
         const { spawn } = require('child_process');
         const os = require('os');
-        const argTemplate = {action: undefined, command: undefined, user: undefined};
+        const argTemplate = {action: undefined, command: undefined, user: undefined, id: 'default', autostart: true};
         const allArgKeys = Object.keys(a);
         let args = {};
         for (const arg of allArgKeys) {
@@ -31,6 +31,7 @@ module.exports.autoadmin = async (a = {}) => {
                 resolve(true);
             });
         }
+        const id = args['id'];
         switch(args['action']) {
             case undefined:
                 console.error('No action was passed.');
@@ -43,13 +44,18 @@ module.exports.autoadmin = async (a = {}) => {
                 }
                 const command = args['command'];
                 const user = args['user'] === undefined ? os.userInfo().username : args['user'];
-                runAndResolve(`schtasks /create /tn "autoadmin" /tr ${escape([command])} /sc ONLOGON /IT /ru ${escape([user])} /RL HIGHEST /F`);
+                const autostart = args['autostart'];
+                if (autostart) {
+                    runAndResolve(`schtasks /create /tn "autoadmin-${escape([id])}" /tr ${escape([command])} /sc ONLOGON /IT /ru ${escape([user])} /RL HIGHEST /F`);
+                } else {
+                    runAndResolve(`schtasks /create /tn "autoadmin-${escape([id])}" /tr ${escape([command])} /sc ONEVENT /EC Application /MO *[System/EventID=-1000] /IT /ru ${escape([user])} /RL HIGHEST /F`);
+                }
                 break;
             case 'unhook':
-                runAndResolve(`schtasks /Delete /tn "autoadmin" /F`);
+                runAndResolve(`schtasks /Delete /tn "autoadmin-${escape([id])}" /F`);
                 break;
             case 'run':
-                runAndResolve(`schtasks /run /tn "autoadmin"`);
+                runAndResolve(`schtasks /run /tn "autoadmin-${escape([id])}"`);
                 break;
         }
     });
